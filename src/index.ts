609672560764790c9feb7f35f5c84a921525afaf
Otim Isaac <isaacomega16@gmail.com>
2024-12-17 23:04:58 +0300

    register task router in project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import taskRouter  from "./routers/tasks";
 
 const app = express();
+
+app.use(express.json());
+app.use("/tasks", taskRouter);
+
 
 app.listen (8001, () => {
     console.log("Server is running on port 8001");
