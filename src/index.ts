f99ee02f19f64999abbfe3b8dc93c5404f8c2db1
Otim Isaac <isaacomega16@gmail.com>
2024-12-18 15:12:32 +0300

    gracefully disconnect prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import taskRouter  from "./routers/tasks";
+import prisma from "./utils/prismaClient";
 
 const app = express();
 
 app.use(express.json());
 app.use("/tasks", taskRouter);
 
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
+  
+process.on('SIGTERM', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
 
-app.listen (8001, () => {
+  const PORT = process.env.PORT || 3000;
+
+app.listen (PORT, () => {
     console.log("Server is running on port 8001");
 });
