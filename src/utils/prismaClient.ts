5dffaa5b2bc81258f8811651c132ec28ea732715
Otim Isaac <isaacomega16@gmail.com>
2024-12-18 14:58:12 +0300

    generate prisma client

---
+++ src/utils/prismaClient.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// Ensure the Prisma Client is only instantiated once in development.
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // Optional: Enable logging for debugging
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+
+export default prisma
