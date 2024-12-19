fe74c8809b2d413e94c5b5162fc06de21f4b0720
Otim Isaac <isaacomega16@gmail.com>
2024-12-19 10:29:48 +0300

    update task table colums

---
+++ prisma/migrations/20241219072628_update_completed_field_name/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isCompleted` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Task` DROP COLUMN `isCompleted`,
+    ADD COLUMN `completed` BOOLEAN NOT NULL DEFAULT false;
