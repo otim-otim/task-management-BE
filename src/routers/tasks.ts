8a37216154b337ec2fc6388510356c5e51b1fec3
Otim Isaac <isaacomega16@gmail.com>
2024-12-19 09:51:51 +0300

    handle errors in router and services

--- src/routers/tasks.ts
+++ src/routers/tasks.ts
@@ -1,17 +1,35 @@
 import { Router } from "express";
 import { ITaskCreateDTO , ITaskUpdateDTO } from "../types";
-import { getTasks } from "../services/tasks";
+import { getTasks, createTask } from "../services/tasks";
+import {celebrate, Joi} from 'celebrate'
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    const tasks = getTasks();
-    res.send(tasks);
+router.get("/", async (req, res) => {
+    try {
+        const tasks = await getTasks();
+        res.send({tasks});
+        
+    } catch (error) {
+        res.send({error});
+    }
 });
 
-router.post("/", (req, res) => {
-    const { title, color } = req.body as ITaskCreateDTO
-    res.send("Create Task");
+router.post("/", celebrate({
+    body: Joi.object({
+        title: Joi.string().required(),
+        color: Joi.string().required()
+    })
+}),
+    async (req, res) => {
+        try {
+            const taskReq = req.body as ITaskCreateDTO
+            const task = await createTask(taskReq)
+            res.send({task});
+            
+        } catch (error) {
+            res.send({error});
+        }
 });
 
 
--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -1,12 +1,26 @@
 import  prisma  from '../utils/prismaClient';
-
+import { ETaskColor, ITaskCreateDTO } from '../types';
 
 
 
 export async function getTasks() {
     try {
-        
         return await prisma.task.findMany();
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        throw new Error('Failed to fetch tasks');
+    }
+}
+
+export async function createTask(request : ITaskCreateDTO) {
+    const {title, color} = request
+    try {
+        return await prisma.task.create({
+            data: {
+                title: title,
+                color: color
+            }
+        });
     } catch (error) {
         console.log(error);
     }
